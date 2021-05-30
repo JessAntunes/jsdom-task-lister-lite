d2c98bc86f792fa6b6b4c4b4d4bcc6616e6f4afb
Jessica Antunes <jessantunes@protonmail.com>
2021-05-30 19:03:22 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.querySelector('form');
-  
-  form.addEventListener('submit', function(event){
+  let form = document.getElementById('create-task-form')
+  let submit = document.querySelector("input[type=submit]")
+
+  form.addEventListener('submit', function(event) {
+    addNewTaskToList();
     event.preventDefault();
-  
-    let inputTag = document.querySelector('input#new-task-description');
-    let inputValue = inputTag.value;
-
-
-    let newLi = document.createElement('li');
-    newLi.innerText = inputValue;
-
-    let ulTag = document.querySelector('ul#tasks');
-    ulTag.append(newLi);
-    inputTag.value = '';
-
-  })
+    clearTaskInput();
+  });
 });
+
+function addNewTaskToList() {
+  let task = document.getElementById('new-task-description').value
+  let taskList = document.getElementById('tasks')
+  let newLi = document.createElement('li')
+
+  let newTask = taskList.appendChild(newLi)
+  newTask.innerHTML = task;
+}
+
+function clearTaskInput() {
+  document.getElementById('new-task-description').value = ""
+}
